fd49469f58e8e9980c765bb807a1c7ad707bd40b
Morten <monoe18@student.sdu.dk>
2021-09-30 18:32:19 +0200

    Update Projects

---
+++ LifecycleExample/app/src/main/java/com/example/exampleapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.exampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("Test", "CREATED")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Test", "STARTED")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Test", "RESUMED")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Test", "PAUSED")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Test", "STOPPED")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Test", "DESTROYED")
+    }
+}
