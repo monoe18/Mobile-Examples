00a40022d9f8582e8e99627b050348952b1dd3a2
Morten <monoe18@student.sdu.dk>
2021-09-30 19:06:05 +0200

    Intent Example added

---
+++ IntentExample/app/src/main/java/com/example/intentexample/DisplayMessageActivity.kt
@@ -0,0 +1,19 @@
+package com.example.intentexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+    }
+}
